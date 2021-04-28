48b31e4e476922f0be83aa68baaa108615256a80
José Ruí Sumner Borda Hurtado <bordarui@gmail.com>
2021-04-28 17:04:47 -0500

    remove unused namespace

--- src/Routes/Index.php
+++ src/Routes/Index.php
@@ -2,8 +2,6 @@
 
 namespace Escalafon\Routes;
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 class Index
